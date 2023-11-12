6787d7f2c8c2ed6a645bcea3100bdab642484e3a
henry <127354681+henryzitos@users.noreply.github.com>
2023-11-12 20:56:09 -0300

    Delete PregaoLG2/src/entity/Ordem.java

--- PregaoLG2/src/entity/Ordem.java
+++
@@ -1,6 +0,0 @@
-package entity;
-
-public class Ordem {
-    //Uma classe que representa as ordens de compra e venda, com atributos como investidor,
-    //tipo de ordem (compra ou venda), ativo, quantidade, preço, etc.
-}
